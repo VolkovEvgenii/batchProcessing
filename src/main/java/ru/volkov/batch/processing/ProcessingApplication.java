ad595a3d88a4ead81e061c8e793b46e7a9b2425f
evgeniy volkov <volkov.e.n@gmail.com>
2021-03-10 12:53:43 +0300

    Add props

--- src/main/java/ru/volkov/batch/processing/ProcessingApplication.java
+++ src/main/java/ru/volkov/batch/processing/ProcessingApplication.java
@@ -1,9 +1,11 @@
 package ru.volkov.batch.processing;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class ProcessingApplication {
 
     public static void main(String[] args) {
